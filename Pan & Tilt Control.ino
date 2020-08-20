99f7f05943375fd74c528f741fbab4c7effc160e
WIRED AI <69744049+WIRED-AI@users.noreply.github.com>
2020-08-21 00:13:58 +0700

    Updated comments

--- Pan & Tilt Control.ino
+++ Pan & Tilt Control.ino
@@ -1,62 +1,68 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
+//Pan & tilt control by WIRED-AI
 
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
+//load servo library
 #include <Servo.h>
 
-Servo yservo, zservo;  // create servo object to control a servo
+Servo yservo, zservo;  // create servo object to control  servo motors
 // twelve servo objects can be created on most boards
 
-int ypos = 0;    // variable to store the servo position
-int zpos = 0;    // variable to store the servo position
+int ypos = 0;    // variable to store the servo y position
+int zpos = 0;    // variable to store the servo z position
 
 
 
 void setup() {
+//using PWM pins 9 & 10 for triggering servo motors
   yservo.attach(9);
   zservo.attach(10);
-  // attaches the servo on pin 9 to the servo object
+  // attaches the servo on pin 9 & 10  to the servo objects
 }
 
 void loop() {
  
+ //set range from 0 to 180
 for(zpos = 0; zpos <= 180; zpos += 10){
   
   zservo.write(zpos);
-  
+ 
+  //set range from 20 to 60
 for(ypos = 20; ypos <=60; ypos += 10) {
 
   yservo.write(ypos);
+ 
+ //allow delay to avoid sudden movements
   delay(250);
 }
- 
+ //set range from 60 to 20
 for(ypos = 60; ypos >=20; ypos -= 10) {
 
   yservo.write(ypos);
+ 
+ //allow delay to avoid sudden movements
   delay(250);
 }
   
 
 }
-
+//set range from 180 to 0
 for(zpos = 180; zpos >= 0; zpos -= 10){
   
   zservo.write(zpos);
-  
+ 
+  //set range from 20 to 60
 for(ypos = 20; ypos <=60; ypos += 10) {
 
   yservo.write(ypos);
+ 
+ //allow delay to avoid sudden movements
   delay(250);
 }
- 
+ //set range from 60 to 20
 for(ypos = 60; ypos >=20; ypos -= 10) {
 
   yservo.write(ypos);
+ 
+ //allow delay to avoid sudden movements
   delay(250);
 }
 }
